ffc7eb1408f0697da71741afcb0017134bb6cc47
Immanuel Bierer <u0534204@utah.edu>
2023-12-07 20:54:08 -0700

    Graph has colors RGB mapped to axes XYZ.

--- app/src/main/cpp/Graph.h
+++ app/src/main/cpp/Graph.h
@@ -22,20 +22,23 @@ public:
             ES_VERSION +
             "layout(location = " STRV(POSITION_ATTRIBUTE_LOCATION) ") in vec3 pos;\n"
             "layout(location = " STRV(NORMAL_ATTRIBUTE_LOCATION) ") in vec3 normal;\n"
+            "out vec3 vPosition;\n"
             "out vec3 vNormal;\n"
             "uniform mat4 mvp;\n"
             "void main() {\n"
             "    gl_Position = mvp * vec4(pos, 1.0);\n"
+            "    vPosition = pos;\n"
             "    vNormal = normal;\n"
             "}\n";
 
     const string FRAGMENT_SHADER =
             ES_VERSION +
             "precision mediump float;\n"
+            "in vec3 vPosition;\n"
             "in vec3 vNormal;\n"
             "out vec4 outColor;\n"
             "void main() {\n"
-            "    outColor = vec4(1.0f, 0.0f, 0.0f, 1.0f); \n"
+            "    outColor = vec4(0.025f * vPosition + vec3(0.5), 1.0f); \n"
             "}\n";
 
     Graph();
