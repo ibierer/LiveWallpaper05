09b2f8ce3db13344d8f272e4b480519b11110346
Immanuel Bierer <u0534204@utah.edu>
2023-12-24 15:59:29 -0700

    Fixed the commented-out instruction that calls dynamicallyGenerateMandelbrotWithVertexShader.

--- app/src/main/cpp/DrawWithFragmentShaderView.cpp
+++ app/src/main/cpp/DrawWithFragmentShaderView.cpp
@@ -18,7 +18,7 @@ void DrawWithFragmentShaderView::render(){
 
     // Option to regenerate the texture each frame.
     if(getFrameCount() > 0){
-        //texture = FBO::dynamicallyGenerateMandelbrotWithVertexShader(temporaryCubeMapFBO, this);
+        //texture = Texture::dynamicallyGenerateMandelbrotWithVertexShader(fbo, this);
     }
 
     glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
