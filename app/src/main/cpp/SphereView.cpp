6f8b8107de5f90c830cdc8ef19a48295773a535b
Immanuel Bierer <u0534204@utah.edu>
2023-12-24 18:49:39 -0700

    Added class SphereView which draws the top hexagon of a red soccer ball.

--- app/src/main/cpp/SphereView.cpp
+++ app/src/main/cpp/SphereView.cpp
@@ -4,6 +4,45 @@
 
 #include "SphereView.h"
 
+VertexNormal* generateSoccerBallVertices(const float& radius) {
+    const float goldenRatio = (1.0f + std::sqrt(5.0f)) / 2.0f;
+
+    VertexNormal* vertices = (VertexNormal*)malloc(1000 * sizeof(VertexNormal));
+    int count = 0;
+
+    vertices[count++].v = vec3(0.0f, 0.0f, 1.0f);
+    for(int i = 0; i < 6; i++){
+        const float theta = M_PI * i / 3.0;
+        const float phi = 1.0f / 9.0f * M_PI;
+        const float twentyDegreesInRadians = M_PI / 9.0;
+        const float sineOfTwentyDegrees = sinf(twentyDegreesInRadians);
+        vertices[count++].v = vec3(sineOfTwentyDegrees * cosf(theta), sineOfTwentyDegrees * sinf(theta), cosf(phi));
+    }
+
+    // Set normals and set scale according to radius parameter.
+    for(int i = 0; i < count; i++) {
+        vertices[i].n = vertices[i].v;
+        vertices[i].v *= radius;
+    }
+
+    return vertices;
+}
+
+uvec3* generateSoccerBallIndices() {
+    uint* indices = (uint*)malloc(1000 * sizeof(uvec3));
+
+    int count = 0;
+
+    for(int i = 0; i < 6; i++){
+        indices[count++] = 0;
+        indices[count++] = i + 1;
+        indices[count++] = (i + 1) % 6 + 1;
+    }
+
+    //return (uvec3*)realloc(indices, count * sizeof(uint));
+    return (uvec3*)indices;
+}
+
 SphereView::SphereView() : View(){
     mProgram = createProgram(VERTEX_SHADER.c_str(), FRAGMENT_SHADER.c_str());
 }
@@ -31,22 +70,27 @@ void SphereView::render(){
             GL_FALSE,
             (GLfloat*)&mvp);
 
-    VertexNormal vertices[3] = {
+
+    /*VertexNormal vertices[3] = {
             {vec3(1.0f, 0.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f)},
             {vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f)},
             {vec3(1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 0.0f)}
     };
     for(int i = 0; i < sizeof(vertices) / sizeof(VertexNormal); i++){
         vertices[i].n = normalize(cross(vertices[2].v - vertices[1].v, vertices[1].v - vertices[0].v));
-    }
-    uvec3 indices[1] = {
+    }*/
+    VertexNormal* vertices = generateSoccerBallVertices(1.0f);
+    /*uvec3 indices[1] = {
             uvec3(0, 1, 2)
-    };
+    };*/
+    uvec3* indices = generateSoccerBallIndices();
     glEnableVertexAttribArray(POSITION_ATTRIBUTE_LOCATION);
     glEnableVertexAttribArray(NORMAL_ATTRIBUTE_LOCATION);
     glVertexAttribPointer(POSITION_ATTRIBUTE_LOCATION, 3, GL_FLOAT, GL_FALSE, sizeof(VertexNormal), (const GLvoid*)&vertices[0].v);
     glVertexAttribPointer(NORMAL_ATTRIBUTE_LOCATION, 3, GL_FLOAT, GL_FALSE, sizeof(VertexNormal), (const GLvoid*)&vertices[0].n);
-    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, indices);
+    glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, indices);
     glDisableVertexAttribArray(POSITION_ATTRIBUTE_LOCATION);
     glDisableVertexAttribArray(NORMAL_ATTRIBUTE_LOCATION);
+    free(vertices);
+    free(indices);
 }
