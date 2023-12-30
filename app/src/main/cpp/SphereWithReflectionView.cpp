481af45a4e0c1f55a56306cba56d048dd1a70401
Immanuel Bierer <u0534204@utah.edu>
2023-12-30 14:26:49 -0700

    SphereWithReflectionView renders a reflective sphere with a fractal background.

--- app/src/main/cpp/SphereWithReflectionView.cpp
+++ app/src/main/cpp/SphereWithReflectionView.cpp
@@ -5,11 +5,14 @@
 #include "SphereWithReflectionView.h"
 
 SphereWithReflectionView::SphereWithReflectionView() : View() {
+    mProgram = createProgram(_BACKGROUND_VERTEX_SHADER.c_str(), _BACKGROUND_FRAGMENT_SHADER.c_str());
+    _mProgram = createProgram(_REFLECTION_VERTEX_SHADER.c_str(), _REFLECTION_FRAGMENT_SHADER.c_str());
     mReflectionProgram = createProgram(REFLECTION_VERTEX_SHADER.c_str(), REFLECTION_FRAGMENT_SHADER.c_str());
     mBackgroundProgram = createProgram(BACKGROUND_VERTEX_SHADER.c_str(), BACKGROUND_FRAGMENT_SHADER.c_str());
-    cubeMap = CubeMap::createSimpleTextureCubemap();
+    //environmentMap = CubeMap::createSimpleTextureCubemap();
+    environmentMap = SphereMap(Texture::DefaultImages::MANDELBROT, 16384, 16384, this);
     sphereVAO = VertexArrayObject(Sphere(1.0f, 100));
-    environmentTriangleVAO = VertexArrayObject(cubeMap.environmentTriangleVertices, sizeof(cubeMap.environmentTriangleVertices) / sizeof(PositionXYZ));
+    environmentTriangleVAO = VertexArrayObject(environmentMap.environmentTriangleVertices, sizeof(environmentMap.environmentTriangleVertices) / sizeof(PositionXYZ));
 }
 
 SphereWithReflectionView::~SphereWithReflectionView(){
@@ -17,7 +20,7 @@ SphereWithReflectionView::~SphereWithReflectionView(){
 }
 
 void SphereWithReflectionView::render(){
-    glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+    /*glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
@@ -56,7 +59,52 @@ void SphereWithReflectionView::render(){
             1,
             GL_FALSE,
             (GLfloat*)&inverseViewProjection);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap.getTextureId());
+    glBindTexture(GL_TEXTURE_CUBE_MAP, environmentMap.getTextureId());
+    //glActiveTexture(GL_TEXTURE0);
+
+    environmentTriangleVAO.draw();*/
+
+
+    glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    Matrix4<float> translation;
+    translation = translation.Translation(Vec3<float>(0.0f, 0.0f, 10.0f * (zoom - 1.0f)));
+    Matrix4<float> rotation;
+    rotation = Matrix4<float>(quaternionTo3x3(rotationVector));
+    Matrix3<float> inverse3x3Transpose;
+    Matrix4<float> view = translation * rotation;
+    Matrix4<float> mvp = orientationAdjustedPerspective * view;
+    inverse3x3Transpose = rotation.GetSubMatrix3().Identity();
+    Matrix4<float> cameraTransformation = rotation.GetInverse() * view;
+
+    glUseProgram(_mProgram);
+    glUniformMatrix4fv(
+            glGetUniformLocation(_mProgram, "mvp"),
+            1,
+            GL_FALSE,
+            (GLfloat*)&mvp);
+    glUniformMatrix4fv(
+            glGetUniformLocation(_mProgram, "viewTransformation"),
+            1,
+            GL_FALSE,
+            (GLfloat*)&cameraTransformation);
+
+    sphereVAO.draw();
+
+
+    Matrix4<float> inverseViewProjection = (orientationAdjustedPerspective * rotation).GetInverse();
+
+    glUseProgram(mProgram);
+    glUniformMatrix4fv(
+            glGetUniformLocation(mProgram, "inverseViewProjection"),
+            1,
+            GL_FALSE,
+            (GLfloat*)&inverseViewProjection);
+    glBindTexture(GL_TEXTURE_2D, environmentMap.getTextureId());
     //glActiveTexture(GL_TEXTURE0);
 
     environmentTriangleVAO.draw();
--- app/src/main/cpp/SphereWithReflectionView.h
+++ app/src/main/cpp/SphereWithReflectionView.h
@@ -11,6 +11,8 @@
 class SphereWithReflectionView : public View {
 public:
 
+    GLuint _mProgram;
+
     GLuint mReflectionProgram;
 
     GLuint mBackgroundProgram;
@@ -19,7 +21,75 @@ public:
 
     VertexArrayObject environmentTriangleVAO;
 
-    CubeMap cubeMap;
+    EnvironmentMap environmentMap;
+
+    const string _REFLECTION_VERTEX_SHADER =
+            ES_VERSION +
+            "layout(location = " STRV(POSITION_ATTRIBUTE_LOCATION) ") in vec3 pos;\n"
+            "layout(location = " STRV(NORMAL_ATTRIBUTE_LOCATION) ") in vec3 normal;\n"
+            "uniform mat4 mvp;\n"
+            "uniform mat4 viewTransformation;\n"
+            "out vec3 direction;\n"
+            "out vec3 vNormal;\n"
+            "void main() {\n"
+            "    gl_Position = mvp * vec4(pos, 1.0);\n"
+            "    direction = (viewTransformation * vec4(pos, 1.0)).xyz;\n"
+            "    vNormal = normal;\n"
+            "}\n";
+
+    const string _REFLECTION_FRAGMENT_SHADER =
+            ES_VERSION +
+            "precision mediump float;\n"
+            "uniform sampler2D environmentTexture;\n"
+            "in vec3 direction;\n"
+            "in vec3 vNormal;\n"
+            "out vec4 outColor;\n"
+            "vec2 directionToSphereMapUV(vec3 direction) {\n"
+            "    // Normalize the direction vector\n"
+            "    vec3 normalizedDirection = normalize(direction);\n"
+            "    // Calculate polar angle (theta) and azimuthal angle (phi)\n"
+            "    float theta = acos(normalizedDirection.z); // polar angle\n"
+            "    float phi = atan(normalizedDirection.y, normalizedDirection.x); // azimuthal angle\n"
+            "    // Map angles to UV coordinates\n"
+            "    float u = phi / (2.0 * 3.14159265359) + 0.5;\n"
+            "    float p = 1.0 - theta / 3.14159265359;\n"
+            "    return vec2(u, p);\n"
+            "}\n"
+            "void main() {\n"
+            "    outColor = texture(environmentTexture, directionToSphereMapUV(reflect(direction, vNormal)));\n"
+            "}\n";
+
+    const string _BACKGROUND_VERTEX_SHADER =
+            ES_VERSION +
+            "layout(location = " STRV(POSITION_ATTRIBUTE_LOCATION) ") in vec3 pos;\n"
+            "uniform mat4 inverseViewProjection;\n"
+            "out vec3 direction;\n"
+            "uniform mat4 mvp;\n"
+            "void main() {\n"
+            "    gl_Position = vec4(pos, 1.0);\n"
+            "    direction = (inverseViewProjection * vec4(pos, 1.0f)).xyz;\n"
+            "}\n";
+
+    const string _BACKGROUND_FRAGMENT_SHADER =
+            ES_VERSION +
+            "precision mediump float;\n"
+            "uniform sampler2D environmentTexture;\n"
+            "in vec3 direction;\n"
+            "out vec4 outColor;\n"
+            "vec2 directionToSphereMapUV(vec3 direction) {\n"
+            "    // Normalize the direction vector\n"
+            "    vec3 normalizedDirection = normalize(direction);\n"
+            "    // Calculate polar angle (theta) and azimuthal angle (phi)\n"
+            "    float theta = acos(normalizedDirection.z); // polar angle\n"
+            "    float phi = atan(normalizedDirection.y, normalizedDirection.x); // azimuthal angle\n"
+            "    // Map angles to UV coordinates\n"
+            "    float u = phi / (2.0 * 3.14159265359) + 0.5;\n"
+            "    float p = 1.0 - theta / 3.14159265359;\n"
+            "    return vec2(u, p);\n"
+            "}\n"
+            "void main() {\n"
+            "    outColor = texture(environmentTexture, directionToSphereMapUV(direction));\n"
+            "}\n";
 
     const string REFLECTION_VERTEX_SHADER =
             ES_VERSION +
