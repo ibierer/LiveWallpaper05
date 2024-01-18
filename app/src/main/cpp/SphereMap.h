6ba92abf2b4c5cb6d18a54af40b62771c9e02024
Immanuel Bierer <u0534204@utah.edu>
2024-01-18 16:19:42 -0700

    Removed a shader from SphereMap that is no longer used.

--- app/src/main/cpp/SphereMap.h
+++ app/src/main/cpp/SphereMap.h
@@ -9,19 +9,6 @@
 class SphereMap : public EnvironmentMap {
 public:
 
-    const string directionToSphereMapUV =
-            "vec2 SPHERE_MAP_TEXTURE_FUNCTION(vec3 direction) {\n"
-            "    // Normalize the direction vector\n"
-            "    vec3 normalizedDirection = normalize(direction);\n"
-            "    // Calculate polar angle (theta) and azimuthal angle (phi)\n"
-            "    float theta = acos(normalizedDirection.z); // polar angle\n"
-            "    float phi = atan(normalizedDirection.y, normalizedDirection.x); // azimuthal angle\n"
-            "    // Map angles to UV coordinates\n"
-            "    float u = phi / (2.0 * 3.14159265359) + 0.5;\n"
-            "    float p = 1.0 - theta / 3.14159265359;\n"
-            "    return vec2(u, p);\n"
-            "}\n";
-
     SphereMap();
 
     SphereMap(Texture &texture);
