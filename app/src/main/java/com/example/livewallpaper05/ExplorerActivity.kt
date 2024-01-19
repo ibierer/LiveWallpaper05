e85c3f40dc04a113cfd48cfb16f9b10ceea1c580
joels <joels@THILANDRIUS>
2024-01-18 17:59:11 -0700

    Updated profile table to match ER diagram

--- app/src/main/java/com/example/livewallpaper05/ExplorerActivity.kt
+++ app/src/main/java/com/example/livewallpaper05/ExplorerActivity.kt
@@ -15,6 +15,10 @@ class ExplorerActivity : AppCompatActivity() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_explorer)
 
+        // Check if user is logged in or signed up, if not send user to login/signup screen
+
+        // query to set profile object
+        // if they are signed up, query for explore wallpapers via recommendation algorithm and send those to the UI
 
     }
 }
--- app/src/main/java/com/example/livewallpaper05/profiledata/ProfileTable.kt
+++ app/src/main/java/com/example/livewallpaper05/profiledata/ProfileTable.kt
@@ -4,16 +4,23 @@ import android.graphics.Bitmap
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import java.time.LocalDate
 
 @Entity(tableName = "profile_table")
 data class ProfileTable (
+    @field:ColumnInfo(name = "uid")
+    @field:PrimaryKey(autoGenerate = true)
+    var uid: Int = 0,
     @field:ColumnInfo(name = "username")
-    @field:PrimaryKey
     var username: String,
+    @field:ColumnInfo(name = "name")
+    var name: String,
     @field:ColumnInfo(name = "bio")
     var bio: String,
-    @field:ColumnInfo(name = "uid")
-    var uid: Int,
+    @field:ColumnInfo(name = "DateCreated")
+    var DateCreated : LocalDate,
     @field:ColumnInfo(name = "profilepic", typeAffinity = ColumnInfo.BLOB)
     var profilepic: ByteArray
 )
+
