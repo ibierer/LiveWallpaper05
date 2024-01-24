3c89e12191c7cde946b00d7c605652556d60cc90
Cameron Steensma <cameronsteensma@gmail.com>
2024-01-24 10:29:00 -0700

    added url for database running on AWS into ExplorerActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("org.postgresql:postgresql:42.3.1")
+    implementation("org.postgresql:postgresql:42.7.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
--- app/src/main/java/com/example/livewallpaper05/ExplorerActivity.kt
+++ app/src/main/java/com/example/livewallpaper05/ExplorerActivity.kt
@@ -1,13 +1,15 @@
 package com.example.livewallpaper05
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
-import java.sql.DriverManager
+import java.sql.*
 import java.time.LocalDateTime
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.SQLException
 
 class ExplorerActivity : AppCompatActivity() {
 
@@ -24,27 +26,29 @@ class ExplorerActivity : AppCompatActivity() {
         mTestButton?.setOnClickListener {
             // write aws test code here -------------
             val tmpImg = ByteArray(0)
-            val url = "jdbc:postgresql://localhost:5432/LiveWallzDB"
-            try {
-                val connection = DriverManager
-                    .getConnection(url, "postgres", "password")
+            Log.d("LiveWallpaper05", "In on Click Listener")
+            val url = "jdbc:postgresql://localhost:5432/postgres"
+            val connection = DriverManager.getConnection(url, "postgres", "UtahUtesLiveWallz!")
                 // check connection is valid here
-                Log.d("LiveWallpaper05", connection.isValid(0).toString())
-                val query = connection.prepareStatement("SELECT * FROM users WHERE username = test")
-                val dateCreated = LocalDateTime.now().toString()
-                val insertQuery =
-                    connection.prepareStatement("INSERT INTO users(username, name, bio, date_created, profile_pic) VALUES('Cam', 'Cameron Steensma, 'The Greatest', $dateCreated, null);")
-                val insertRes = insertQuery.executeQuery()
-                Log.d("LiveWallpaper05", insertRes.toString())
-                val result = query.executeQuery()
-                Log.d("LiveWallpaper05", result.toString())
+            Log.d("LiveWallpaper05", connection.isValid(0).toString())
+
+            Log.d("LiveWallpaper05", "Driver manager connected!")
+            val query = connection.prepareStatement("SELECT * FROM users WHERE username = test")
+            Log.d("LiveWallpaper05", "GOT HERE!")
+            val dateCreated = LocalDateTime.now().toString()
+            val insertQuery =
+            connection.prepareStatement("INSERT INTO users(username, name, bio, date_created) VALUES('Cam', 'Cameron Steensma, 'The Greatest', $dateCreated, null);")
+            val insertRes = insertQuery.executeQuery()
+            Log.d("LiveWallpaper05", insertRes.toString())
+            val result = query.executeQuery()
+            Log.d("LiveWallpaper05", result.toString())
 //                profileDao.updateProfileData(ProfileTable(result.getInt("uid"), result.getString("username"), result.getString("name"),
 //                    result.getString("bio"), result.getString("date_created"), result.getBytes("profile_pic")))
             }
-            catch(exception: Exception){
-                Log.d("LiveWallpaper05", exception.toString())
-            }
+//            catch(exception: Exception){
+//                Log.d("LiveWallpaper05", exception.toString())
+//            }
 
         }
-    }
+
 }
