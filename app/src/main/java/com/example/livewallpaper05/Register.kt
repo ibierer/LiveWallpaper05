9eb04b7fc1f60212fe6adaf0cbaf764d2f91b78c
Cameron Steensma <cameronsteensma@gmail.com>
2024-02-23 12:48:24 -0700

    User is able to register using firebase with a valid email format and password that is >= 6 characters in length.

--- app/src/main/java/com/example/livewallpaper05/Register.kt
+++ app/src/main/java/com/example/livewallpaper05/Register.kt
@@ -1,11 +1,63 @@
 package com.example.livewallpaper05
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.livewallpaper05.databinding.ActivityRegisterBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+    private lateinit var etEmail: TextInputEditText
+    private lateinit var etPassword: TextInputEditText
+    private lateinit var registerButton: Button
+    private lateinit var loginTextView: TextView
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        val binding: ActivityRegisterBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mAuth = FirebaseAuth.getInstance()
+        etEmail = binding.email
+        etPassword = binding.password
+        registerButton = binding.registerButton
+        loginTextView = binding.loginNowTV
+
+        loginTextView.setOnClickListener{
+            val loginPageIntent = Intent(this, Login::class.java)
+            startActivity(loginPageIntent)
+            finish()
+        }
+
+        registerButton.setOnClickListener{
+            val email : String = etEmail.text.toString()
+            val password : String = etPassword.text.toString()
+            if (TextUtils.isEmpty(email)){
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)){
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Account created!", Toast.LENGTH_SHORT,).show()
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                    }
+                }
+
+        }
+
     }
 }
