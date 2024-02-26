1722923bf45dba2b1a164a7aa9a8ef02603b7d73
Cameron Steensma <cameronsteensma@gmail.com>
2024-02-26 14:24:05 -0700

    Registration page now properly notifies the user if a username already exists (our unique key for db) and does not register the user into Firebase authentication table.

--- app/src/main/java/com/example/livewallpaper05/Register.kt
+++ app/src/main/java/com/example/livewallpaper05/Register.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Database
 import com.example.livewallpaper05.databinding.ActivityRegisterBinding
 import com.google.android.material.textfield.TextInputEditText
@@ -15,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.sql.DriverManager
 import java.sql.SQLException
 import java.util.Properties
@@ -48,50 +50,60 @@ class Register : AppCompatActivity() {
         loginTextView = binding.loginNowTV
         etUsername = binding.usernameEditText
 
-        loginTextView.setOnClickListener{
+        loginTextView.setOnClickListener {
             val loginPageIntent = Intent(this, Login::class.java)
             startActivity(loginPageIntent)
             finish()
         }
 
-        registerButton.setOnClickListener{
-            val email : String = etEmail.text.toString()
-            val password : String = etPassword.text.toString()
-            val username : String = etUsername.text.toString()
-            if (TextUtils.isEmpty(email)){
+        registerButton.setOnClickListener {
+            val email: String = etEmail.text.toString()
+            val password: String = etPassword.text.toString()
+            val username: String = etUsername.text.toString()
+            if (TextUtils.isEmpty(email)) {
                 Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if (TextUtils.isEmpty(password)){
+            if (TextUtils.isEmpty(password)) {
                 Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if (TextUtils.isEmpty(username)){
+            if (TextUtils.isEmpty(username)) {
                 Toast.makeText(this, "Enter username", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-
-            mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "Account created!", Toast.LENGTH_SHORT,).show()
-                        insertIntoUsers(username, email)
-                        // Navigate to the login page
-                        val loginPageIntent = Intent(this, Login::class.java)
-                        startActivity(loginPageIntent)
-                        finish()
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+            var userExists = false
+            lifecycleScope.launch {
+                userExists = isUserInDB(username)
+            }
+            if (userExists) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Account created!", Toast.LENGTH_SHORT).show()
+                            insertIntoUsers(username, email)
+                            // Navigate to the login page
+                            val loginPageIntent = Intent(this, Login::class.java)
+                            startActivity(loginPageIntent)
+                            finish()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     }
-                }
+            }
+            else{
+                Toast.makeText(this, "Username already exists!", Toast.LENGTH_SHORT)
+                    .show()
+            }
 
         }
 
     }
-    fun insertIntoUsers(username: String, name: String){
+
+    fun insertIntoUsers(username: String, name: String) {
         GlobalScope.launch(Dispatchers.IO) {
-            // write aws test code here -------------
             val jdbcConnectionString = ProfileActivity.DatabaseConfig.jdbcConnectionString
             try {
                 Class.forName("com.mysql.jdbc.Driver").newInstance()
@@ -103,23 +115,55 @@ class Register : AppCompatActivity() {
 
                 DriverManager.getConnection(jdbcConnectionString, connectionProperties)
                     .use { conn -> // this syntax ensures that connection will be closed whether normally or from exception
-                        Log.d("LiveWallpaper05", "Connected to database")
                         val useDbQuery = "USE myDatabase;"
                         val statement = conn.prepareStatement(useDbQuery)
                         statement.execute()
-                        Log.d("LiveWallpaper05", "Pt A REACHED")
                         val insertQuery = "INSERT INTO users (username, name) VALUES (?, ?);"
                         val preparedStatement = conn.prepareStatement(insertQuery)
                         preparedStatement.setString(1, username)
                         preparedStatement.setString(2, name)
                         preparedStatement.executeUpdate()
-                        Log.d("LiveWallpaper05", "Pt B REACHED")
                         conn.close()
                     }
             } catch (e: SQLException) {
                 Log.e("LiveWallpaper05", e.printStackTrace().toString())
                 Log.d("LiveWallpaper05", e.toString())
             }
+        }
+    }
+
+    suspend fun isUserInDB(username: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            val jdbcConnectionString = ProfileActivity.DatabaseConfig.jdbcConnectionString
+            var userExists = false
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance()
+                // connect to mysql server
+                val connectionProperties = Properties()
+                connectionProperties["user"] = ProfileActivity.DatabaseConfig.dbUser
+                connectionProperties["password"] = ProfileActivity.DatabaseConfig.dbPassword
+                connectionProperties["useSSL"] = "false"
+
+                DriverManager.getConnection(jdbcConnectionString, connectionProperties).use { conn ->
+                    // this syntax ensures that the connection will be closed whether normally or from an exception
+
+                    val useDbQuery = "USE myDatabase;"
+                    val useDbStatement = conn.prepareStatement(useDbQuery)
+                    useDbStatement.execute()
+                    val checkUserQuery = "SELECT COUNT(*) FROM users WHERE username = ?;"
+                    val checkUserStatement = conn.prepareStatement(checkUserQuery)
+                    checkUserStatement.setString(1, username)
+                    val resultSet = checkUserStatement.executeQuery()
+
+                    resultSet.next()
+                    val userCount = resultSet.getInt(1)
+                    userExists = userCount > 0
+                }
+            } catch (e: SQLException) {
+                Log.d("CHECKDBUSER", e.message.toString())
+            }
+            userExists
         }
     }
 
