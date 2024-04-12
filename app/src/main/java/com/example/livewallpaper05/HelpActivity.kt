a41ec394c456f5b0492e9d8df5077765dcd884d0
Cameron Steensma <cameronsteensma@gmail.com>
2024-04-11 20:03:59 -0600

    Created layout for the Help page, need to hook up screen recordings.

--- app/src/main/java/com/example/livewallpaper05/HelpActivity.kt
+++ app/src/main/java/com/example/livewallpaper05/HelpActivity.kt
@@ -2,10 +2,16 @@ package com.example.livewallpaper05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class HelpActivity : AppCompatActivity() {
+    private lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        videoView = findViewById(R.id.faq1VideoView)
+        // Prepare the video file or URL
+        //val videoPath = "android.resource://${packageName}/${R.raw.your_video_file}" // For a video file in the raw folder
     }
 }
