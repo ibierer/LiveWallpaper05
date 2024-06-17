ae1390983cba78aa3036a2d68ce0bfae51e0de00
Immanuel Bierer <u0534204@utah.edu>
2024-06-16 19:02:51 -0600

    Upgraded several dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.livewallpaper05"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.livewallpaper05"
@@ -49,32 +49,33 @@ dependencies {
 
     implementation("com.github.yukuku:ambilwarna:2.0.1")
 
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-common-ktx:20.4.2")
-    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
-    var room_version = "2.4.3"
-    var activity_version = "1.6.1"
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-common-ktx:21.0.0")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
+    val roomVersion = "2.6.1"
+    val activityVersion = "1.7.0"
+    val ktxVersion = "2.8.2"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("org.postgresql:postgresql:42.7.1")
 
-    implementation("androidx.appcompat:appcompat:$activity_version")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("androidx.appcompat:appcompat:$activityVersion")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.firebaseui:firebase-ui-auth:7.2.0")
-    implementation("androidx.activity:activity-ktx:$activity_version")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // room database dependencies
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
     annotationProcessor("android.arch.persistence.room:compiler:1.1.1")
     //annotationProcessor("androidx.room:room-compiler:$room_version")
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     // postgreSQL JDBC dependencies
     implementation("org.postgresql:postgresql:42.7.1")
@@ -82,11 +83,11 @@ dependencies {
     implementation("com.firebase:firebase-client-android:2.5.0")
 
     // view model dependencies
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$ktxVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$ktxVersion")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    kapt("androidx.lifecycle:lifecycle-compiler:2.2.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.0")
+    kapt("androidx.lifecycle:lifecycle-compiler:$ktxVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
