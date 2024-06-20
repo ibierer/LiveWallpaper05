f53354b8de2ca85cac40e6051b17d86f8672cb9a
Immanuel Bierer <u0534204@utah.edu>
2024-06-19 19:31:43 -0600

    targetSdk is increased from 33 to 34.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vizbox4d"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.001"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
