7a9c086f1a9fff0be785f65dc83df58679ffb680
Immanuel Bierer <u0534204@utah.edu>
2024-06-25 19:29:37 -0600

    Notification bar is always visible now.

--- app/src/main/java/com/vizbox4d/PreviewActivity.kt
+++ app/src/main/java/com/vizbox4d/PreviewActivity.kt
@@ -351,9 +351,6 @@ class PreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        @Suppress("DEPRECATION")
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_preview)
 
         // get view window from GLES3JNIView
