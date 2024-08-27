cd153bd78dd3ea92e693c4df908cc784101f0436
Immanuel Bierer <u0534204@utah.edu>
2024-08-27 17:35:39 -0600

    Ad banner no longer fades out.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xendovo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.010"
+        versionCode = 11
+        versionName = "1.011"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
